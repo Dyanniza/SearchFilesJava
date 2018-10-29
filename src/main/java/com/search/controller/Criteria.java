3e0963a8cfe9ae352601f16010887e878da79d67
Zeila Benavidez <zeilitav@gmail.com>
2018-10-29 00:24:23 -0400

    Adding a new Criteria class that will search by an object given

---
+++ src/main/java/com/search/controller/Criteria.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ * This software is the confidential and proprietary information of  Jalasoft, ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into  with Jalasoft.
+ */
+
+package main.java.com.search.controller;
+
+public class Criteria
+{
+    private String path;
+    private String fileName;
+    private String ext;
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public String getPath()
+    {
+        return this.path;
+    }
+
+    public void setFileName(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public String getFileName()
+    {
+        return this.fileName;
+    }
+
+    /*
+    public void setExt(String ext)
+
+    {
+        this.ext = Searchv.getPanel().getext();
+    }
+
+    public String getExt()
+    {
+        return this.ext;
+    }
+
+*/
+}
