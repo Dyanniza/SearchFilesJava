0177ea96053c9f79b57f5fc089e108b8c84835b5
Dyanniza <ceci.chiqui@gmail.com>
2018-11-07 19:15:55 -0400

    Adding Test classes and  unit test for each class for controller and view

---
+++ src/test/java/com/search/controller/ControllerTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ * This software is the confidential and proprietary information of  Jalasoft, ("Confidential Information").
+ *  You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement you entered into  with Jalasoft.
+ *
+ */
+
+package com.search.controller;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest extends Controller {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
